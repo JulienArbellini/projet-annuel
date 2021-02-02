53ba27999568237cf67cfeb265c12190abd76943
Lea Ifergan <leaifergan15@gmail.com>
2021-02-02 18:47:48 +0100

    Initial commit

---
+++ www/Controllers/Security.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App;
+
+use App\Core\Security as coreSecurity;
+use App\Core\Database;
+use App\Core\View;
+use App\Core\Form;
+use App\Core\ConstantManager;
+use App\Models\User;
+
+class Security{
+
+
+	public function defaultAction(){
+		echo "controller security action default";
+	}
+
+
+	public function registerAction(){
+
+
+		$user = new User();
+		$view = new View("register");
+		$form = $user->buildFormRegister();
+		$view->assign("form", $form);
+
+		if(!empty($_POST)){
+			$errors = Form::validator($_POST, $form);
+
+			if(empty($errors)){
+				
+				$user->setFirstname($_POST["firstname"]);
+				$user->setLastname($_POST["lastname"]);
+				$user->setEmail($_POST["email"]);
+				$user->setPwd($_POST["pwd"]);
+				$user->setCountry("fr");
+				$user->save();
+
+			}else{
+				$view->assign("formErrors", $errors);
+			}
+
+		}
+		
+		
+
+	}
+
+	public function loginAction(){
+		echo "controller security action login";
+	}
+
+	public function logoutAction(){
+		echo "controller security action logout";
+	}
+
+	public function listofusersAction(){
+
+		$security = new coreSecurity(); 
+		if(!$security->isConnected()){
+			die("Error not authorized");
+		}
+
+		echo "Là je liste tous les utilisateurs";
+	}
+
+}
