3cc92c7a85b5434182fa863f51a10fe5dcbeb8d1
JulienArbellini <arbellini.j@gmail.com>
2021-02-15 22:04:45 +0100

    Fichiers déjà existants que j'ai modifié

--- www/Views/dashboard_view.php
+++ www/Views/dashboard_view.php
@@ -1,5 +1,15 @@
-<section>
-	<h2>Page du dashboard</h2>
-</section>
+<div id="content">
+	<!-- <section>
+		<h2>Page du dashboard</h2>
+	</section>
 
-<button class="button">Cliquez ici</button>
+	<div class="progress">
+		<div class="progress-value"></div>
+	</div>
+
+	<div class="search-box">
+		<input type="text" placeholder="Rechercher ..." >
+	</div> -->
+</div>
+
+
