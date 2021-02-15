a893d2f35355971eb0d5acbc55432c41453171e7
Lea Ifergan <leaifergan15@gmail.com>
2021-02-15 19:38:59 +0100

    Feature button

--- www/Views/Templates/back_tpl.php
+++ www/Views/Templates/back_tpl.php
@@ -5,6 +5,7 @@
 		<title>Template de Back</title>
 		<meta name="description" content="ceci est la page de template">
 		<link rel="stylesheet" href="framework/dist/main.css">
+        <script type="text/javascript" src="dist/main.js"></script>
 	</head>
 	<body>
 		<header>
--- www/Views/dashboard_view.php
+++ www/Views/dashboard_view.php
@@ -3,3 +3,4 @@
 </section>
 
 <button class="button">Cliquez ici</button>
+<button class="button">Cliquez ici</button>
