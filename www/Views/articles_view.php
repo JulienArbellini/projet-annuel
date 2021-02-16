9525d366f2802daa5d76a05b6033a761fef562d5
WarunyRajendran <waruny@hotmail.fr>
2021-02-16 16:44:25 +0100

    code css article

--- www/Views/articles_view.php
+++ www/Views/articles_view.php
@@ -1,16 +1,18 @@
 <h1>Articles</h1>
 
-<div class="#">
-    <img src="../../framework/img/add.png" width="12" height="12"> 
-    <p>Ajouter un article</p>
+<div class="article-search-group">
+    <div class="add-article">
+        <div class="picture"><img src="../../framework/img/add.png" width="12" height="12"></div>
+        <div class="picture-text"><p>Ajouter un article</p></div>
+    </div>
+
+    <div class="search-box">
+        <input type="text" placeholder="Rechercher un article">
+    </div>
 </div>
 
-<div class="search-box">
-    <input type="text" placeholder="Rechercher un article">
-</div>
-
-<div class="shadow-box-square">
-    <div class="container-flexbox">
+<div class="shadow-box-square--article">
+    <div class="name-column">
         <p><strong>Nom article</strong></p>
         <p><strong>Dernières modifications</strong></p>
         <p><strong>Auteur</strong></p>
@@ -22,8 +24,8 @@
         <a href="#" class="link">Mon premier article</a>
         <p>le 10/09/2020 à 10h00</p>
         <p>John Doe</p>
-        <img src="../../framework/img/pen-edit.svg" alt="pen-edit" width="12" height="12">
-        <img src="../../framework/img/cross-delete.svg" alt="cross-delete" width="12" height="12">
+        <img src="../../framework/img/pen-edit.svg" alt="pen-edit" width="15" height="15">
+        <img src="../../framework/img/cross-delete.svg" alt="cross-delete" width="15" height="15">
     </div>
 
     <div class="separator"></div>
@@ -32,8 +34,8 @@
         <a href="#" class="link">Mon deuxième article</a>
         <p>le 15/11/2020 à 18h40</p>
         <p>John Doe</p>
-        <img src="../../framework/img/pen-edit.svg" alt="pen-edit" width="12" height="12">
-        <img src="../../framework/img/cross-delete.svg" alt="cross-delete" width="12" height="12">
+        <img src="../../framework/img/pen-edit.svg" alt="pen-edit" width="15" height="15">
+        <img src="../../framework/img/cross-delete.svg" alt="cross-delete" width="15" height="15">
     </div>
 
     <div class="separator"></div>
@@ -42,8 +44,8 @@
         <a href="#" class="link">Mon troisième article</a>
         <p>le 21/01/2021 à 13h10</p>
         <p>John Doe</p>
-        <img src="../../framework/img/pen-edit.svg" alt="pen-edit" width="12" height="12">
-        <img src="../../framework/img/cross-delete.svg" alt="cross-delete" width="12" height="12">
+        <img src="../../framework/img/pen-edit.svg" alt="pen-edit" width="15" height="15">
+        <img src="../../framework/img/cross-delete.svg" alt="cross-delete" width="15" height="15">
     </div>
 
     <div class="separator"></div>
@@ -52,8 +54,8 @@
         <a href="#" class="link">Mon quatrième article</a>
         <p>le 19/02/2021 à 21h55</p>
         <p>John Doe</p>
-        <img src="../../framework/img/pen-edit.svg" alt="pen-edit" width="12" height="12">
-        <img src="../../framework/img/cross-delete.svg" alt="cross-delete" width="12" height="12">
+        <img src="../../framework/img/pen-edit.svg" alt="pen-edit" width="15" height="15">
+        <img src="../../framework/img/cross-delete.svg" alt="cross-delete" width="15" height="15">
     </div>
 
 
