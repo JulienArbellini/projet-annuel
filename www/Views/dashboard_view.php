f6ec5651737ca6342bd2bf467b8d4725d7b01f9f
Lea Ifergan <leaifergan15@gmail.com>
2021-02-16 16:41:15 +0100

    Component switch button

--- www/Views/dashboard_view.php
+++ www/Views/dashboard_view.php
@@ -1,17 +1,11 @@
-<div id="content">
-	<!-- <section>
-		<h2>Page du dashboard</h2>
-	</section>
 
-	<div class="progress">
-		<div class="progress-value"></div>
-	</div>
+<label class="switch">
+    <input type="checkbox">
+    <span class="slider round"></span>
+</label>
 
-	<div class="search-box">
-		<input type="text" placeholder="Rechercher ..." >
-	</div> -->
-</div>
+<label class="switch">
+    <input type="checkbox" checked>
+    <span class="slider round"></span>
+</label>
 
-
-<button class="button">Cliquez ici</button>
-<button class="button">Cliquez ici</button>
