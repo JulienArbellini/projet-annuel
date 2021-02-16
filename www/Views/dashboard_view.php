fa1ef53969e83bfc55fb42d8659e973e4e694950
Lea Ifergan <leaifergan15@gmail.com>
2021-02-16 18:24:23 +0100

    Rajout de la progress bar

--- www/Views/dashboard_view.php
+++ www/Views/dashboard_view.php
@@ -1,20 +1,23 @@
 
-<label class="switch">
-    <input type="checkbox">
-    <span class="slider round"></span>
-</label>
-
 	<div class="search-box">
 		<input type="text" placeholder="Rechercher ..." >
 	</div>
 	<button class="button">Cliquez ici</button>
 	<button class="button">Cliquez ici</button>
 
+<div class="progress">
+    <div class="progress-value"></div>
+</div>
 
 
 
 <label class="switch">
     <input type="checkbox" checked>
+    <span class="slider round"></span>
+</label>
+
+<label class="switch">
+    <input type="checkbox">
     <span class="slider round"></span>
 </label>
 
