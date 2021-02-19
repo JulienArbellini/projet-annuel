87bd8b2ce8fa4394c7aa240b8da512300a03a7b9
Brindeau Kelig <keligbrindeau@gmail.com>
2021-02-19 15:51:10 +0100

    grille css et adaptation code nav bar+page articles

--- www/Views/articles_view.php
+++ www/Views/articles_view.php
@@ -1,65 +1,63 @@
-<div class="article-search-group">   
-    <h1>Articles</h1>
-    <div class="add-article-search"> 
-        <a href="#"><div class="add-article">
-            <p>+</p>
-            <div class="picture-text"><p>Ajouter un article</p></div>
-        </div></a>
-        <div class="search-box" style="width:339px">
+<div class="row col-m-12">
+    <div class="add-article-search col-s-10 col-m-8 col-m-center">
+        <h1>Articles</h1>
+        <a href="#" class="col-s-10 col-m-2 col-m-push-3 col-m-down-3">
+            <p>+ Ajouter un article</p>
+        </a>
+
+        <div class="search-box">
             <input type="text" placeholder="Rechercher un article">
         </div>
     </div>
+
+    <div class="shadow-box-square col-s-10 col-m-8 col-m-center">
+        <div class="name-column">
+            <div class="col-m-3 col-m-push-1"><p><strong>Nom article</strong></p></div>
+            <div class="col-m-3 col-m-push-1"><p><strong>Dernières modifications</strong></p></div>
+            <div class="col-m-3 col-m-push-1"><p><strong>Auteur</strong></p></div>
+        </div>
+
+        <div class="separator"></div>
+
+        <div class="container-flexbox">
+            <a href="#" class="link">Mon premier article</a>
+            <p>le 10/09/2020 à 10h00</p>
+            <p>John Doe</p>
+            <a href="#"><img src="../../framework/img/pen-edit.svg" alt="pen-edit" width="15" height="15"></a>
+            <a href="#"><img src="../../framework/img/cross-delete.svg" alt="cross-delete" width="15" height="15"></a>
+        </div>
+
+        <div class="separator"></div>
+
+        <div class="container-flexbox">
+            <a href="#" class="link">Mon deuxième article</a>
+            <p>le 15/11/2020 à 18h40</p>
+            <p>John Doe</p>
+            <a href="#"><img src="../../framework/img/pen-edit.svg" alt="pen-edit" width="15" height="15"></a>
+            <a href="#"><img src="../../framework/img/cross-delete.svg" alt="cross-delete" width="15" height="15"></a>
+        </div>
+
+        <div class="separator"></div>
+
+        <div class="container-flexbox">
+            <a href="#" class="link">Mon troisième article</a>
+            <p>le 21/01/2021 à 13h10</p>
+            <p>John Doe</p>
+            <a href="#"><img src="../../framework/img/pen-edit.svg" alt="pen-edit" width="15" height="15"></a>
+            <a href="#"><img src="../../framework/img/cross-delete.svg" alt="cross-delete" width="15" height="15"></a>
+        </div>
+
+        <div class="separator"></div>
+
+        <div class="container-flexbox">
+            <a href="#" class="link">Mon quatrième article</a>
+            <p>le 19/02/2021 à 21h55</p>
+            <p>John Doe</p>
+            <a href="#"><img src="../../framework/img/pen-edit.svg" alt="pen-edit" width="15" height="15"></a>
+            <a href="#"><img src="../../framework/img/cross-delete.svg" alt="cross-delete" width="15" height="15"></a>
+        </div>
+
+    
+    </div>
+
 </div>
-
-<div class="shadow-box-square">
-    <div class="name-column">
-        <p><strong>Nom article</strong></p>
-        <p><strong>Dernières modifications</strong></p>
-        <p><strong>Auteur</strong></p>
-    </div>  
-
-    <div class="separator"></div>
-
-    <div class="container-flexbox">
-        <a href="#" class="link">Mon premier article</a>
-        <p>le 10/09/2020 à 10h00</p>
-        <p>John Doe</p>
-        <a href="#"><img src="../../framework/img/pen-edit.svg" alt="pen-edit" width="15" height="15"></a>
-        <a href="#"><img src="../../framework/img/cross-delete.svg" alt="cross-delete" width="15" height="15"></a>
-    </div>
-
-    <div class="separator"></div>
-
-    <div class="container-flexbox">
-        <a href="#" class="link">Mon deuxième article</a>
-        <p>le 15/11/2020 à 18h40</p>
-        <p>John Doe</p>
-        <a href="#"><img src="../../framework/img/pen-edit.svg" alt="pen-edit" width="15" height="15"></a>
-        <a href="#"><img src="../../framework/img/cross-delete.svg" alt="cross-delete" width="15" height="15"></a>
-    </div>
-
-    <div class="separator"></div>
-
-    <div class="container-flexbox">
-        <a href="#" class="link">Mon troisième article</a>
-        <p>le 21/01/2021 à 13h10</p>
-        <p>John Doe</p>
-        <a href="#"><img src="../../framework/img/pen-edit.svg" alt="pen-edit" width="15" height="15"></a>
-        <a href="#"><img src="../../framework/img/cross-delete.svg" alt="cross-delete" width="15" height="15"></a>
-    </div>
-
-    <div class="separator"></div>
-
-    <div class="container-flexbox">
-        <a href="#" class="link">Mon quatrième article</a>
-        <p>le 19/02/2021 à 21h55</p>
-        <p>John Doe</p>
-        <a href="#"><img src="../../framework/img/pen-edit.svg" alt="pen-edit" width="15" height="15"></a>
-        <a href="#"><img src="../../framework/img/cross-delete.svg" alt="cross-delete" width="15" height="15"></a>
-    </div>
-
-
-</div>
-
-
-
