ff6651dee0a9ea82c135fbeee2689fd8de83b85e
Waruny_Rajendran <wrajendran@myges.fr>
2021-07-11 20:20:39 +0200

    add required

--- www/install.php
+++ www/install.php
@@ -17,19 +17,19 @@
             <input type="hidden" name="etape" value="1" />
             <div class="form-group">
                 <label class="control-label" for="bdd-name">Nom de la base de données</label>
-                <input class="input" id="bdd-name" name="bdd-name" type="text">
+                <input class="input" id="bdd-name" name="bdd-name" type="text" required>
             </div>
             <div class="form-group">
                 <label class="control-label" for="bdd-user-name">Nom d'utilisateur</label>
-                <input class="input" id="bdd-user-name" name="bdd-user-name" type="text">
+                <input class="input" id="bdd-user-name" name="bdd-user-name" type="text" required>
             </div>
             <div class="form-group">
                 <label class="control-label" for="bdd-user-pwd">Mot de passe</label>
-                <input class="input" id="bdd-user-pwd" name="bdd-user-pwd" type="password">
+                <input class="input" id="bdd-user-pwd" name="bdd-user-pwd" type="password" required>
             </div>
             <div class="form-group">
                 <label class="control-label" for="bdd-host">Hôte</label>
-                <input class="input" id="bdd-host" name="bdd-host" type="text">
+                <input class="input" id="bdd-host" name="bdd-host" type="text" required>
             </div>
             <div class="form-group sgbd">
                 <p>Attention ! Le SGBD utilisé doit être MySQL.</p>
